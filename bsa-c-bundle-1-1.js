f14037346d0eb76eaa00488bb1bdedc4e1b2f915
businesssuccess <159882838+businesssuccess@users.noreply.github.com>
2024-02-14 10:28:08 +1100

    Add files via upload

---
+++ bsa-c-bundle-1-1.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/businesssuccess/bsa-cf2-carousel/bsa-c-1-2.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css" />
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/businesssuccess/bsa-cf2-carousel/bsa-c-1-2.css"/>
